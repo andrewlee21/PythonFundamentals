dc111a705d991a21a3e428a3e000757ea100083d
andrewlee21 <andrewlee21@sbcglobal.net>
2021-08-17 11:19:07 -0700

    c1 Exercise

---
+++ cc1.py
@@ -0,0 +1,20 @@
+#Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 2.5
+print(type(b))
+
+c = "Greg"
+print(type(c))
+
+d = a+b
+print(d)
+
+
+
+
+
