cf69e33b1e36d2f41aed35858f2a2fc00dd9bd15
andrewlee21 <andrewlee21@sbcglobal.net>
2021-08-28 01:13:11 -0700

    cc Dictionaries

---
+++ Week 3/cc_dictionary.py
@@ -0,0 +1,16 @@
+inches_snow = {"Monday": 2, "Tuesday": 4, "Wednesday": 5}
+
+def print_total_snowfall(inches_snow):
+    total_inches = 0
+    for inches in inches_snow:
+        total_inches += inches_snow[inches]
+    print("Total snowfall inches:",total_inches)
+
+print_total_snowfall(inches_snow)
+
+inches_snow["Thursday"] = int(input("How many inches fell on Thursday?"))
+    
+print_total_snowfall(inches_snow)
+
+
+
--- Week 3/inter_dicts.py
+++ Week 3/inter_dicts.py
@@ -1,2 +1,13 @@
-for state in state_capitals:
-    print(state)
+
+state_capitals = {"Washington": "Olympia", "Oregon": "Salem", "California": "Sacramento"}
+# print(len(state_capitals))
+# print(state_capitals["Washington"])
+
+
+# for state in state_capitals:
+#     print(state)
+
+for city in state_capitals.values():
+    print(city)
+
+    
