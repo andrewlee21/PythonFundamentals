abbff7fa7686a60db7b4608d76d891a2003af444
andrewlee21 <andrewlee21@sbcglobal.net>
2021-09-06 09:44:56 -0700

    ws3

--- Week 3/using_sets.py
+++ Week 3/using_sets.py
@@ -1,25 +1,26 @@
-# numbers_set = {1, 2, 3, 4, 4}
+# # numbers_set = {1, 2, 3, 4, 4}
 
-# print(numbers_set)
-# # numbers_set = {1, 2, 3, 4, 4}       # duplicate values removed
+# # print(numbers_set)
+# # # numbers_set = {1, 2, 3, 4, 4}       # duplicate values removed
 
-# # numbers_set = {1, 2, 3, 4, [5, 6]}
+# # # numbers_set = {1, 2, 3, 4, [5, 6]}
 
-# numbers_set = {1, 2, 3, 4, (5, 6)}
-# print(numbers_set)
+# # numbers_set = {1, 2, 3, 4, (5, 6)}
+# # print(numbers_set)
 
-words_set = {"Alpha", "Bravo", "Charlie"}
-# abcd = ""
-# for word in words_set:
-#     abcd += word
-# print(abcd)
+# words_set = {"Alpha", "Bravo", "Charlie"}
+# # abcd = ""
+# # for word in words_set:
+# #     abcd += word
+# # print(abcd)
 
-# if "Alpha" in words_set:
-#     print("Alpha is in set")
-# else:
-#     print("Alpha not in set")
+# # if "Alpha" in words_set:
+# #     print("Alpha is in set")
+# # else:
+# #     print("Alpha not in set")
 
-words_set.add("Delta")
-print(words_set)
-words_set.discard("Bravo")
-print(words_set)
+# words_set.add("Delta")
+# print(words_set)
+# words_set.discard("Bravo")
+# print(words_set)
+
